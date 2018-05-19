da72054ac9d4ff0db945a6b9db61276501e18345
Tyler Alder <tyler.b.alder@gmail.com>
2018-05-19 01:16:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,5 +9,7 @@ function whisper(string) {
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase) { 
     return "I can't hear you!"
+  } else if(string === string.toUpperCase) {
+    return ""
   }
 }
