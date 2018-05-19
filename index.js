38cf71f4ba0e29e43298e1fa0569e4c2f17ecf41
Tyler Alder <tyler.b.alder@gmail.com>
2018-05-19 01:49:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  console.log(string.toUpperCase)
+  console.log("Hello")
 }
 
 function logWhisper(string) {
-  console.log(string.toLowerCase)
+  console.log("hello")
 }
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase) { 
